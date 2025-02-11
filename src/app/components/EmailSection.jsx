5f9d5e0199ed8ea4202cf4c2da3e14f023ece2c1
sultangithub04 <sult_04@yahoo.com>
2025-02-11 20:26:41 +0600

    added

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -1,9 +1,6 @@
 "use client";
 import React, { useState } from "react";
-import GithubIcon from "../../../public/github-icon.svg";
-import LinkedinIcon from "../../../public/linkedin-icon.svg";
-import Link from "next/link";
-import Image from "next/image";
+
 import { FaWhatsapp, FaEnvelope, FaPhoneAlt, FaMapMarkerAlt } from "react-icons/fa";
 
 const EmailSection = () => {
@@ -79,14 +76,7 @@ const EmailSection = () => {
             </div>
           </div>
         </div>
-        <div className="socials flex flex-row gap-2">
-          <Link href="https://github.com/sultangithub04">
-            <Image src={GithubIcon} alt="Github Icon" />
-          </Link>
-          <Link href="https://linkedin.com/in/sultan-ph10">
-            <Image src={LinkedinIcon} alt="Linkedin Icon" />
-          </Link>
-        </div>
+       
       </div>
       <div>
         {emailSubmitted ? (
