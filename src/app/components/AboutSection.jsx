261bd453cd0ee479ed703c42bc37183355cbb4d7
sultangithub04 <sult_04@yahoo.com>
2025-09-13 13:08:29 +0600

    some updated

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -15,14 +15,11 @@ const TAB_DATA = [
     title: "Skills",
     id: "skills",
     content: (
-      <section className="py-12 bg-white px-6">
-        <h2 className="text-3xl font-bold text-center text-gray-900">
-          🚀 Skills That Set Me Apart
-        </h2>
-        <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-6 mt-8">
+      <section className="py-12">
+        <div className="grid md:grid-cols-2 lg:grid-cols-4">
           {Object.entries(skills).map(([category, list]) => (
-            <div key={category} className="p-6 rounded-xl shadow bg-gray-50">
-              <h3 className="text-xl font-semibold text-indigo-600">{category}</h3>
+            <div key={category} className="">
+              <h3 className="font-semibold text-indigo-600">{category}</h3>
               <ul className="mt-3 space-y-1 text-gray-700">
                 {list.map((skill) => (
                   <li key={skill}>• {skill}</li>
@@ -78,13 +75,7 @@ const AboutSection = () => {
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-lg">
-            Hello! I’m Sultan, a dedicated Full-Stack Web Developer who loves building modern, user-friendly, and scalable web applications. From designing clean, responsive interfaces to developing powerful backend systems, I enjoy every step of turning ideas into reality.
-
-            I work with a wide range of technologies including HTML, CSS, JavaScript, TypeScript, TailwindCSS, Firebase, JWT, Axios, React.js, Next.js, Node.js, Express.js, Prisma, Docker, AWS, GraphQL, Redux, MongoDB, Mongoose, SQL, PostgreSQL, and many more.
-
-            Beyond just writing code, I focus on clean architecture, performance, and best practices to deliver solutions that not only work but also grow with your needs.
-
-            When I’m not coding, I enjoy exploring new tools, learning emerging technologies, and refining my skills to stay ahead in the ever-evolving tech world.
+            I’m Sultan, a passionate Full-Stack Web Developer skilled in frontend and backend technologies like React.js, Next.js, Node.js, Express.js, Prisma, PostgreSQL, MongoDB, Docker, AWS, and many more — crafting ideas into engaging online experiences.
           </p>
           <div className="flex flex-row justify-start mt-8">
             <TabButton
